a6411751f5453381d667bad7ec6d4a4938abe658
Roman Pavlenko <proman9973@gmail.com>
2022-07-26 14:23:01 +0300

    finished header

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     semi: 0,
     'no-proto': 0,
     'no-unused-vars': 0,
+    'jsx-a11y/anchor-is-valid': 0,
 
     // TypeScript
     '@typescript-eslint/no-unused-vars': ['error'],
